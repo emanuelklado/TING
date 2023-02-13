9e81a11fa7346b8e3abf3cccbc984919e24204d2
emanuelklado <emanuelklado@msn.com>
2023-02-12 22:12:01 -0300

    implementando função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
+
+def exists_word(word, instance: Queue):
+    registers = []
+    reg_list = []
+    file_add = False
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        lines =  file["linhas_do_arquivo"]
+
+        for l in range(len(lines)):
+            if word.lower() in lines[l].lower():
+                reg_list.append({"linha": l + 1})
+                file_add = True
+
+        if file_add:
+            registers.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": reg_list
+            })
+
+    return registers
 
 
 def search_by_word(word, instance):
