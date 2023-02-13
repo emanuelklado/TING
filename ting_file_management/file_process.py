5b4aec0978f0bcdbe341f23bae69ed7f41569293
emanuelklado <emanuelklado@msn.com>
2023-02-12 21:18:09 -0300

    implementando file_metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -28,4 +28,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        f = instance.queue[position]
+        return print(f)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
